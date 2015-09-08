d570f0063dc3df186f737b291a4bea0b1f67a62c
Yuriy Perevoznikov <yuriless@gmail.com>
2015-09-08 12:33:03 +0300

    Updated example code for Kohana

--- examples/index.php
+++ examples/index.php
@@ -5,49 +5,13 @@
 //
 use \YPEarlyCache\Factory;
 
-$manager = Factory::getManager(Factory::PLATFORM_KOHANA, 'path\to\config.php', $_GET, $_SERVER, $_COOKIE);
+$manager = Factory::getManager(Factory::KOHANA, 'path\to\config.php', $_GET, $_SERVER, $_COOKIE);
 if (!$manager->flushCacheIfAble()) {
 
-    // generate $content, $memoType, $responseCode
-    $manager->setCache($content, $memoType, $responseCode);
+	//
+    // TODO: generate $content, $headers, $responseCode
+	//
+
+    $manager->setCache($content, $headers, $responseCode);
 
 }
-
-
-//
-// old style
-//
-$enable_early_cache = true;
-if (isset($_GET['early_cache']) && 'false' == $_GET['early_cache']) {
-    $enable_early_cache = false;
-}
-if (isset($_COOKIE['authautologin'])) {
-    $enable_early_cache = false;
-}
-
-require_once("early_cache.php");
-$uri = $_SERVER["REQUEST_URI"];
-if ($enable_early_cache) {
-    if (earlyCacheNeedCache($uri)) {
-        $text = earlyCacheGetHtml($uri);
-
-        if (substr_count($text, "Страница не найдена :(")) {
-            $text = false;
-        }
-    }
-}
-
-$cacheSeconds = (int)(earlyCacheGetCacheSeconds(earlyCacheGetType($uri)));
-if (isset($text) && false !== $text) {
-    header("Content-Type: text/html");
-    header("Cache-Control: max-age={$cacheSeconds}");
-} else {
-
-    // get $text content here
-
-    if ($cacheSeconds && earlyCacheNeedCache($uri)) {
-        earlyCacheSetHtml($uri, $text);
-    }
-}
-
-echo $text;
