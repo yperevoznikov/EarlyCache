02f016f31d91f18830386a30aa1c979758229943
Yuriy Perevoznikov <yuriless@gmail.com>
2015-11-26 21:49:58 +0300

    Array based config

--- src/config/ArrayConfig.php
+++ src/config/ArrayConfig.php
@@ -5,4 +5,38 @@ use YPEarlyCache\Contracts\IConfig;
 class ArrayConfig extends BaseConfig implements IConfig
 {
 
+	public function __construct($params){
+		$this->checkRequiredParams($params);
+
+		$this->enabled = $params['enabled'];
+		$this->rules = $params['rules'];
+		$this->cacheDir = $params['cache_dir'];
+
+		if (isset($params['cookie_no_cache'])) {
+			if (is_array($params['cookie_no_cache'])) {
+				$this->cookieNoCache = $params['cookie_no_cache'];
+			} else {
+				$this->cookieNoCache = array($params['cookie_no_cache']);
+			}
+		}
+
+		if (isset($params['minimize_html'])) {
+			$this->minimizeHtml = $params['minimize_html'];
+		}
+
+		if (isset($params['debug'])) {
+			$this->debug = $params['debug'];
+		}
+
+	}
+
+	private function checkRequiredParams($params) {
+		foreach ($this->requiredConfigParams as $requiredConfigParam) {
+			if (!isset($params[$requiredConfigParam])) {
+				$message = "Required config param in PHP Required Config missed: $requiredConfigParam\n";
+				throw new \Exception($message);
+			}
+		}
+	}
+
 }
---
+++ tests/Config/ArrayConfigTest.php
@@ -0,0 +1,60 @@
+<?php namespace YPEarlyCache\Config;
+
+class ArrayConfigTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var PhpRequiredConfig
+	 */
+	private $cfg1;
+
+	/**
+	 * @var PhpRequiredConfig
+	 */
+	private $cfg2;
+
+	/**
+	 * @var PhpRequiredConfig
+	 */
+	private $cfg3;
+
+	public function setUp(){
+		$arr1 = require(dirname(dirname(__FILE__)) . '/data-example/php-require-config-1.php');
+		$this->cfg1 = new ArrayConfig($arr1);
+
+		$arr2 = require(dirname(dirname(__FILE__)) . '/data-example/php-require-config-2.php');
+		$this->cfg2 = new ArrayConfig($arr2);
+
+		$arr3 = require(dirname(dirname(__FILE__)) . '/data-example/php-require-config-3.php');
+		$this->cfg3 = new ArrayConfig($arr3);
+	}
+
+	public function testIsEnabled(){
+		$this->assertTrue($this->cfg1->isEnabled());
+		$this->assertFalse($this->cfg2->isEnabled());
+	}
+
+	public function testGetRules(){
+		$this->assertTrue(is_array($this->cfg1->getRules()));
+	}
+
+	public function testGetCacheDir(){
+		$this->assertTrue(is_string($this->cfg1->getCacheDir()));
+	}
+
+	public function testGetCookieNoCache() {
+		$this->assertTrue(is_array($this->cfg1->getCookieNoCache()));
+		$this->assertTrue(is_array($this->cfg2->getCookieNoCache()));
+	}
+
+	public function testNeedMinimizeHtml(){
+		$this->assertTrue($this->cfg1->needMinimizeHtml());
+		$this->assertFalse($this->cfg2->needMinimizeHtml());
+	}
+
+	public function testIsDebug(){
+		$this->assertTrue($this->cfg1->isDebug());
+		$this->assertFalse($this->cfg2->isDebug());
+		$this->assertFalse($this->cfg3->isDebug());
+	}
+
+}
